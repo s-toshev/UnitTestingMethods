a59db5f9ff6929d1f1196b417bf05a8595b3f73c
s-toshev <simeon.toshev@yahoo.bg>
2024-02-14 21:03:16 +0200

    Add project files.

---
+++ TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void Test_CalculateFactorial_InputZero_ReturnsOne()
+    {
+        // TODO: Write your test here...
+    }
+
+    [Test]
+    public void Test_CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        // TODO: Write your test here...
+    }
+}
